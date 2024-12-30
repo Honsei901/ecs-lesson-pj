7bb66076888dc09ec964361ea6a0ef5c4a0e4c07
Seiya Homma <99792735+Honsei901@users.noreply.github.com>
2024-12-30 11:05:45 +0900

    Update apiClient.ts

--- sample-webapp/app/api/apiClient.ts
+++ sample-webapp/app/api/apiClient.ts
@@ -2,12 +2,12 @@ import axios from 'axios';
 
 /** Test for health check api */
 export const healthCheck = async () => {
-  const { data } = await axios.get('htts://shoppyfinal.com');
+  const { data } = await axios.get('https://shoppyfinal.com');
   return data;
 };
 
 /** Test for RestAPI */
 export const checkRestApi = async () => {
-  const { data } = await axios.get('htts://shoppyfinal.com/api');
+  const { data } = await axios.get('https://shoppyfinal.com/api');
   return data;
 };
